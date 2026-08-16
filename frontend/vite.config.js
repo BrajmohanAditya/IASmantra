66e86b1167e195a8594f83df8c9547ad040a5b25
Your Name <your.email@example.com>
2026-08-16 13:24:25 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["chrome >= 80", "firefox >= 78", "edge >= 80", "defaults"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
